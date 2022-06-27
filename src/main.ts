c883884dd0df3b99e7575ed68d14cf5204380ff0
jean trujillo <jtrujillo@acl.cl>
2022-06-27 18:38:36 -0400

    setting folder docs

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { setupSwagger } from './docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const options = new DocumentBuilder()
-    .setTitle('Hello World')
-    .setDescription('The Hello World API description')
-    .setVersion('1.0')
-    .addTag('helloWorld')
-    .build();
-
-    const helloWorldDocument = SwaggerModule.createDocument(app, options, {
-      include: [AppModule],
-    });
-    SwaggerModule.setup('api/docs', app, helloWorldDocument,{
-      explorer: true,
-      swaggerOptions:{
-        filter: true,
-        showRequestDuration: true,
-      },
-    });
+  setupSwagger(app);
+ 
 
   await app.listen(3000);
 }
