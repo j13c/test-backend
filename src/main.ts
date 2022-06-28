34ed0e33dd4de9a2308e3abdda4393b8947d1c57
Jean Trujillo <jeancarlostrujillo@gmail.com>
2022-06-28 14:53:34 -0400

    Setting Auth JWT, UserModule, install passport-jwt

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/main/app.module';
 import { setupSwagger } from './docs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   setupSwagger(app);
  
-
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -3,26 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { AuthModule } from '../auth';
 import { ConfigModule, ConfigService } from '../config';
+
+const settingORM = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    return {
+      type: configService.get('DB_TYPE'),
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [__dirname + './../**/**.entity{.ts,.js}'],
+      synchronize: configService.get('DB_SYNC') === 'true',
+    } as TypeOrmModuleAsyncOptions;
+  },
+}
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: configService.get('DB_TYPE'),
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
-          entities: [__dirname + './../**/**.entity{.ts,.js}'],
-          synchronize: configService.get('DB_SYNC') === 'true',
-        } as TypeOrmModuleAsyncOptions;
-      },
-    })
+    TypeOrmModule.forRootAsync(settingORM),
+    ConfigModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
