05e6080c37c6f8f4cc22b40eefe6c951496fdd12
jean trujillo <jtrujillo@acl.cl>
2022-06-27 20:58:48 -0400

    Setting ORM, .ENV, Hash, Connection mysql

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/main/app.module';
 import { setupSwagger } from './docs';
 
 async function bootstrap() {
